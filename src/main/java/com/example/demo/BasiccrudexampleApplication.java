460a44a6b3f712bbc80f195f791272ce20b65d7a
pmvivek <pmvivek@hotmail.com>
2021-11-05 19:27:06 +0530

    comments

--- src/main/java/com/example/demo/BasiccrudexampleApplication.java
+++ src/main/java/com/example/demo/BasiccrudexampleApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Basic Spring DAO with JPA implementation
+
 @SpringBootApplication
 public class BasiccrudexampleApplication {
 
